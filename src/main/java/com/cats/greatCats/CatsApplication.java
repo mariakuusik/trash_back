d137c6a73783a3034d092f1d9905a15d5779ef42
mariakuusik <maria.kuusik@gmail.com>
2023-09-25 20:14:44 +0300

    Initial commit

---
+++ src/main/java/com/cats/greatCats/CatsApplication.java
@@ -0,0 +1,13 @@
+package com.cats.greatCats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cats/greatCats/CatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cats.greatCats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
